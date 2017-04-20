357f8d337fc49849cc8d926734d26ff162245a7b
cice <cice@aula1PovMacN09.local>
2017-04-20 11:21:13 +0200

    Initial commit

---
+++ app/src/main/java/com/example/alex/firebasedatabase/User.java
@@ -0,0 +1,18 @@
+package com.example.alex.firebasedatabase;
+
+/**
+ * Created by cice on 20/4/17.
+ */
+
+public class User {
+
+    public String name;
+    public String email;
+
+    public User(){}
+
+    public User(String email, String name) {
+        this.email = email;
+        this.name = name;
+    }
+}
